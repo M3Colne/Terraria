e04a591ac8ffae3d021f3ae30a15f59d0bdaf84e
Gurau Nikolas <lilitroman@yahoo.com>
2020-05-12 03:33:38 +0300

    Made the update function and organized the Player.h file

--- Engine/Player.cpp
+++ Engine/Player.cpp
@@ -12,6 +12,26 @@ void Player::Draw(Graphics& gfx)
 	gfx.DrawTextureChroma(dx, dy, 0, 0, playerTexture.GetWidth(), playerTexture.GetHeight(), playerTexture, Colors::Magenta);
 }
 
+void Player::Update(bool wP, bool sP, bool aP, bool dP, float dt)
+{
+	if (wP)
+	{
+		position.y -= speed * dt;
+	}
+	if (sP)
+	{
+		position.y += speed * dt;
+	}
+	if (aP)
+	{
+		position.x -= speed * dt;
+	}
+	if (dP)
+	{
+		position.x += speed * dt;
+	}
+}
+
 Vec2 Player::GetCamera() const
 {
 	return position - Vec2(float(dx), float(dy)); //This returns cworld
--- Engine/Player.h
+++ Engine/Player.h
@@ -7,9 +7,12 @@ class Player
 private:
 	//Variables
 	const Texture2D playerTexture;
+	Vec2 position;
+	float speed = 1.0f;
+
+	//Constants
 	const int dx = Graphics::ScreenWidth / 2 - playerTexture.GetWidth() / 2;
 	const int dy = Graphics::ScreenHeight / 2 - playerTexture.GetHeight() / 2;
-	Vec2 position;
 private:
 	//Functions
 public:
@@ -18,5 +21,6 @@ public:
 	//Functions
 	Player(const float x, const float y);
 	void Draw(Graphics& gfx);
+	void Update(bool wP, bool sP, bool aP, bool dP, float dt);
 	Vec2 GetCamera() const;
 };
