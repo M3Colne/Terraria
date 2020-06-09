54b8f1a996caf9aed305520cf0f757ab97cb5515
Gurau Nikolas <lilitroman@yahoo.com>
2020-06-09 05:42:38 +0300

    Rule of 3 for CBox

--- Engine/ClickBoxes.h
+++ Engine/ClickBoxes.h
@@ -4,11 +4,15 @@
 
 struct CBox
 {
+	CBox() = default;
 	CBox(const Vei2& p, const Vei2& P)
 		:
 		p0(p),
 		p1(P)
 	{}
+	CBox(const CBox& other) = delete;
+	CBox operator=(const CBox& other) = delete;
+	~CBox() = delete;
 	Vei2 p0;
 	Vei2 p1;
 };
