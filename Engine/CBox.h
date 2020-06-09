66de145d212c01391692d713e6869adba310a958
Gurau Nikolas <lilitroman@yahoo.com>
2020-06-09 21:23:21 +0300

    Added isColliding to CBox

--- Engine/CBox.h
+++ Engine/CBox.h
@@ -4,6 +4,15 @@
 
 struct CBox
 {
+    bool isCollidingWithMouse(Vei2 mVector)
+    {
+        isCollidingWithMouse(mVector.x, mVector.y);
+    }
+    bool isCollidingWithMouse(int x, int y)
+    {
+        return x >= p0.x && x <= p1.x &&
+            y >= p0.y && y <= p1.y;
+    }
     Vei2 p0 = { 400, 300 };
     Vei2 p1 = { 425, 300 };
 };
